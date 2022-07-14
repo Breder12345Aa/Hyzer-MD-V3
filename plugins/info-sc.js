aab5e28bf87be9c7dcd99e7f7386157f63044574
Breder12345Aa <82520506+Breder12345Aa@users.noreply.github.com>
2022-07-15 05:27:54 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -11,10 +11,9 @@ let ye = `@${m.sender.split`@`[0]}`
 let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
-Nih Link Sc Nya Dibawah :
-https://youtu.be/AXPzjQ5jxx8
+Sc : masih tahab pengembangan
 
-_*Jangan Lupa Di Subscribe*_`
+_*Jangan Lupa Donasj*_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
